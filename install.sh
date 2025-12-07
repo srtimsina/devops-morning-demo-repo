e438b7b900d63b66785e5a2dbac3b7a3bf1edbe7
TA Suryaraj <devopsuryaraj@hotmail.com>
2025-12-07 02:00:55 +0000

    check apache package installation status

--- install.sh
+++ install.sh
@@ -2,6 +2,14 @@
 
 echo "Demo script"
 apt update
+status_code=$(dpkg -l | grep apache2)
+if [ status_code -eq 0 ];
+then
+	echo "The package apache2 is already installed"
+else
+	echo "apache2 is not installed, installing it..."
+fi
+
 apt install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
@@ -9,3 +17,8 @@ sudo systemctl status apache2
 # Put some dummy echo message
 echo "hello DevOps" > /var/www/html/index.html
 
+# Check the disk usage status
+df -h
+
+# current date
+date
